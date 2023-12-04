9071de8377770b7c83aaa452dc0310e0773068e2
Bruce Clark <Bruce.clark55@gmail.com>
2023-12-03 19:21:27 -0500

    made changes

--- events/formEvents.js
+++ events/formEvents.js
@@ -53,7 +53,8 @@ const formEvents = (user) => {
         first_name: document.querySelector('#first_name').value,
         last_name: document.querySelector('#last_name').value,
         email: document.querySelector('#email').value,
-        favorite: document.querySelector('#favorite').value,
+        favorite: document.querySelector('#favorite').checked,
+        uid: user.uid
       };
 
       console.warn(payload);
